93f3bd578c1b7ed9e7cc155b7286fff6e57ca43c
pawelb23 <pawelbialek1823@gmail.com>
2022-07-20 01:03:14 +0200

    test commit

---
+++ react-js-test/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return <div><h1>Testowy Komponent</h1></div>
+  }
+}
+
+export default App;
